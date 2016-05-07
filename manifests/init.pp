874a91895090e53e940a574479852ce9a47ea4ff
Jethro Carr <jethro.carr@jethrocarr.com>
2016-05-07 18:50:05 +1200

    Fixing package/service name

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@
 # backups from MacOS. See the README for more information and usage examples.
 
 class timemachine (
-  $package_netatalk   = $::timemachine::params::package_strongswan,
-  $service_netatalk   = $::timemachine::params::service_strongswan,
+  $package_netatalk   = $::timemachine::params::package_netatalk,
+  $service_netatalk   = $::timemachine::params::service_netatalk,
   $manage_firewall_v4 = $::timemachine::params::manage_firewall_v4,
   $manage_firewall_v6 = $::timemachine::params::manage_firewall_v6,
   $volsizelimit       = $::timemachine::params::vpn_name,
